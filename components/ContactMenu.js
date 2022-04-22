d5f950f5a216945fdb7e89df30ee5084d286822f
Adecoded <adebisidrey@gmail.com>
2022-04-22 12:19:38 +0100

    Co-authored-by: AnnieSqq <AnnieSqq@users.noreply.github.com>changes made

--- components/ContactMenu.js
+++ components/ContactMenu.js
@@ -1,19 +1,47 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AntDesign from "react-native-vector-icons/AntDesign";
+
+const items = [
+  {
+    
+    name:"Starred",
+    
+},
+{
+  
+  name:"Drey",
+ photo: require("../assests")
+  
+},
+{
+  
+  name:"Annie",
+  
+},
+  
+{
+  
+  name:"David",
+  
+  
+},
+]
 const ContactMenu = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
+    {items.map((item, index) =>
+
+      <View style={styles.row} key={index}>
 
       <View style={styles.starredIcon}>
 <AntDesign name='star' size={30} color="white" />
       </View>
-
-      </View>
       <Text style={styles.text}>
-      Stared
+      {item.name}
       </Text>
+      </View>
+      )}
     </View>
   )
 }
@@ -23,5 +51,24 @@ export default ContactMenu
 const styles = StyleSheet.create({
   container:{
 
-  }
+  },
+  row:{
+flexDirection:"row",
+marginTop:20,
+alignItems:"center",
+  },
+  starredIcon:{
+backgroundColor:"#333",
+width:55,
+height:55,
+justifyContent:"center",
+alignItems:"center",
+borderRadius:20,
+  },
+  text:{
+color:"white",
+paddingLeft:15,
+fontSize:18,
+
+  },
 })
